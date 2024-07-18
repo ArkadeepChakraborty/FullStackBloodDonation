87027c5fd416adc141e233a14a78b7a6994c5e25
Arkadeep Chakraborty <157718373+ArkadeepChakraborty@users.noreply.github.com>
2024-07-18 21:10:54 +0530

    Add files via upload

---
+++ FullStackBloodDonation/Frontend/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Login from './Login';
+import Gender from './Gender';
+import Abc from './Abc';
+import Menu from './Menu';
+import AddContact from './AddContact';
+import Loginone from './Loginone';
+import NewUser from './NewUser';
+// import ForgetPassword from './ForgetPassword';
+import Dashboard from './Dashboard';
+import Profile from './Profile';
+import Exam from './Exam';
+import BloodyUserRegistration from './BloodyUserRegistration';
+import DonerUser from './DonerUser';
+import DonerUserRegistration from './DonerUserRegistration';
+import UserDashboard from './UserDashboard';
+import Search from './Search';
+import Searchone from './Searchone';
+import Homeone from './Homeone';
+import Slider from './Slider';
+import Main from './Main';
+import BloodUser from './Blooduser';
+import AddProduct from './AddProduct';
+import CartappHome from './CartappHome';
+import Payment from './Payment';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <Main></Main> */}
+<Payment></Payment>
+  
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
