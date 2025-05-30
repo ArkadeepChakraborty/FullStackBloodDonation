da734afff3b2354cd5cf7e6e76c51664970c4837
Arkadeep Chakraborty <157718373+ArkadeepChakraborty@users.noreply.github.com>
2025-05-30 12:46:22 +0530

    Delete Backends directory

--- Backends/AboutSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const aboutSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    points: [{ type: String }],
-});
-
-module.exports = mongoose.model('About', aboutSchema);
